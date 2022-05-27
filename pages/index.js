cf13f779875fb3cfac6f30112c9bfef873ffb24d
tthugy <supermanace7@gmail.com>
2022-05-27 22:43:23 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import profile from './profile'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
+      <Link href="/profile">
+        <a className={styles.link}>profile</a>
+      </Link>
       welcome to myApp 🥸
     </div>
   )
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 
 export default function profile() {
   return (
     <div>
+      <Link href="/">
+        <a>index</a>
+      </Link>
       Profile 🫠!
     </div>
   )
