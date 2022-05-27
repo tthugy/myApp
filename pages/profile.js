b82a1791604da46d1ab5045aa26ea1648b941798
tthugy <supermanace7@gmail.com>
2022-05-27 22:45:35 +0900

    update

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 
 export default function profile() {
   return (
     <div>
       <Link href="/">
-        <a>index</a>
+        <a className={styles.link}>index</a>
       </Link>
       Profile 🫠!
     </div>
