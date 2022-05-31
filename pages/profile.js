c8feaa2a0cc5850588fd9284d7d8b2a79430031b
tthugy <supermanace7@gmail.com>
2022-05-31 21:35:46 +0900

    update

--- pages/profile.js
+++ pages/profile.js
@@ -6,7 +6,7 @@ export default function profile() {
   return (
     <div>
       <Link href="/">
-        <a className={styles.link}>index</a>
+        <a className={styles.link}>Go to index</a>
       </Link>
       <source src="/assets/media.mp4" type="video/mp4"/>
       Profile 🫠!
