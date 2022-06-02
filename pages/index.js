c05fc9493a756d91bf7fd9bcabceccc064e1d176
tthugy <supermanace7@gmail.com>
2022-06-02 22:57:00 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div>
       <Link href="/profile">
-        <a className={styles.link}>Go to profile</a>
+        <a className={styles.link}>Go to profile 🫧</a>
       </Link>
       <Image src="/assets/next.png" width={100} height={100}></Image>
       welcome to my-App 🥸
